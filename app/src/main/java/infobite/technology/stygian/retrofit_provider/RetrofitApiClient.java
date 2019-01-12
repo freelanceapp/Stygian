189e79a617042074f5d3331c1d5c7ca5578ba411
amar <amardeep.infobite@gmail.com>
2019-01-12 16:53:21 +0530

    453

---
+++ app/src/main/java/infobite/technology/stygian/retrofit_provider/RetrofitApiClient.java
@@ -0,0 +1,22 @@
+package infobite.technology.stygian.retrofit_provider;
+
+import infobite.technology.stygian.constant.Constant;
+import infobite.technology.stygian.model.product_responce.ProductModel;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface RetrofitApiClient {
+
+    @GET(Constant.ALL_CATEGORY)
+    Call<ProductModel> allCategory();
+
+
+
+
+
+}
